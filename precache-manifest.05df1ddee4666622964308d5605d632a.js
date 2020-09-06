fe683c769ade5cbd1551eb183cf9abc172f4db2e
sebastiankut <sebastian_kut@yahoo.co.uk>
2020-09-06 21:12:25 +0200

    Updates

---
+++ precache-manifest.05df1ddee4666622964308d5605d632a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f07e6884b33d1bcb0c763edb948d8c",
+    "url": "/to-do-app-react/index.html"
+  },
+  {
+    "revision": "d89812456b80ca75f676",
+    "url": "/to-do-app-react/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "9e80d14a7b0db9c1ef86",
+    "url": "/to-do-app-react/static/js/2.eb5c251e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to-do-app-react/static/js/2.eb5c251e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d89812456b80ca75f676",
+    "url": "/to-do-app-react/static/js/main.5a3f5012.chunk.js"
+  },
+  {
+    "revision": "1c591e22d14622981ce2",
+    "url": "/to-do-app-react/static/js/runtime-main.de3239b8.js"
+  }
+]);
